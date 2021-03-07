9d08cdb977f61e91c197319321438a0ce85231c4
José Gala Naranjo <jgalanar@gmail.com>
2021-03-08 00:15:55 +0100

    Finish welcome text

--- Assets/Scripts/UI/WelcomePrompt.cs
+++ Assets/Scripts/UI/WelcomePrompt.cs
@@ -1,16 +1,47 @@
 ﻿using UnityEngine;
 using TMPro;
 using Joserbala.Serialization;
+using DG.Tweening;
+using UnityEngine.Events;
+using System.Collections;
 
 namespace Joserbala.UI
 {
     public class WelcomePrompt : MonoBehaviour
     {
+        [SerializeField] private float timeStatic;
+        [SerializeField] private float timeToFade;
+        [TextArea(3, 10)]
+        [SerializeField] private string welcomePrompt = "This game makes use of your MyDocuments folder.\n\nYou can find this folder at ";
         [SerializeField] private TextMeshProUGUI initialText;
+        [SerializeField] private UnityEvent startGame;
+
+        private Sequence fadeOut;
+
+        private void Awake()
+        {
+            CreateFadeOutSequence();
+        }
 
         private void Start()
         {
-            initialText.text = "This game makes use of your MyDocuments folder.\n\nYou can find this folder at " + FileManager.SavePath + ".";
+            StartCoroutine(StartFading());
+            initialText.text = welcomePrompt + FileManager.SavePath + ".";
+        }
+
+        IEnumerator StartFading()
+        {
+            yield return new WaitForSeconds(timeStatic);
+            fadeOut.Play();
+        }
+
+        private void CreateFadeOutSequence()
+        {
+            fadeOut = DOTween.Sequence();
+
+            fadeOut.Append(initialText.DOFade(0, timeToFade));
+            fadeOut.AppendCallback(() => gameObject.SetActive(false));
+            fadeOut.AppendCallback(() => startGame?.Invoke());
         }
     }
 }
