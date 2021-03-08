4d01094839a110f3d0a80b6290d1b8bae7ecec4b
José Gala Naranjo <jgalanar@gmail.com>
2021-03-08 01:20:34 +0100

    OnApplicationQuit

---
+++ Assets/Scripts/Utils/ApplicationQuit.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using UnityEngine;
+using Joserbala.Serialization;
+
+namespace Joserbala.Utils
+{
+    public class ApplicationQuit : MonoBehaviour
+    {
+        private void OnApplicationQuit()
+        {
+            Directory.Delete(FileManager.SavePath, true);
+        }
+    }
+}
